601c55fa8533bb565d269da5993e470387529dd0
khundezq <43135583+khundezq@users.noreply.github.com>
2018-09-13 23:50:53 +0700

    Add files via upload

---
+++ BMI.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class BMI{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print ("Enter weight => ");
+        int w = sc.nextInt();
+        System.out.print ("Enter Height => ");
+        int h = sc.nextInt();
+        int b = w/(m*m);
+        if b < 18.50 
+		{System.out.print("Thin");}
+            if b>= 18.50
+		{System.out.print ("Normal");}
+                 if b>=23
+		{System.out.print ("Fat");}
+		
+	}
+}
